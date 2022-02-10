ad72879a0c349bb74d6a00ec9b11fb1cd4047466
rqueiros <ricardo.queiros@gmail.com>
2022-02-10 21:38:43 +0000

    lti upgrade

---
+++ index.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+
+const lti = require("ltijs").Provider;
+
+// Setup
+lti.setup(
+  process.env.LTI_TOOL_KEY,
+  {
+    //url: `mongodb://${process.env.LTI_TOOL_DB_HOST}:${process.env.LTI_TOOL_DB_PORT}/${process.env.LTI_TOOL_DB_NAME}`,
+    url: "mongodb+srv://agniAdmin:agniAdminPass@agni.phbzj.mongodb.net/ltiDb?retryWrites=true&w=majority",
+    connection: {
+      authSource: process.env.LTI_TOOL_DB_AUTH,
+      user: process.env.LTI_TOOL_DB_USERNAME,
+      pass: process.env.LTI_TOOL_DB_PASSWORD,
+    },
+  },
+  {
+    staticPath: path.join(__dirname, "./public"), // Path to static files
+    cookies: {
+      secure: false, // Set secure to true if the testing platform is in a different domain and https is being used
+      sameSite: "", // Set sameSite to 'None' if the testing platform is in a different domain and https is being used
+    },
+    devMode: true, // Set DevMode to true if the testing platform is in a different domain and https is not being used
+  }
+);
+
+// When receiving successful LTI launch redirects to app
+lti.onConnect(async (token, req, res) => {
+  return res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// When receiving deep linking request redirects to deep screen
+lti.onDeepLinking(async (token, req, res) => {
+  return lti.redirect(res, "/lti-tool/deeplink", { newResource: true });
+});
+
+// Setting up routes
+lti.app.use(routes);
+
+// Setup function
+const setup = async () => {
+  console.log(process.env);
+  await lti.deploy({ serverless: true });
+
+  const app = express();
+  app.use("/lti-tool", lti.app);
+  app.listen(process.env.LTI_TOOL_PORT);
+
+  /**
+   * Register platform
+   */
+  const platform = await lti.registerPlatform({
+    url: process.env.MOODLE_URL,
+    name: process.env.LTI_TOOL_NAME,
+    clientId: process.env.LTI_TOOL_CLIENT_ID,
+    authenticationEndpoint: `${process.env.MOODLE_URL}/mod/lti/auth.php`,
+    accesstokenEndpoint: `${process.env.MOODLE_URL}/mod/lti/token.php`,
+    authConfig: {
+      method: "JWK_SET",
+      key: `${process.env.MOODLE_URL}/mod/lti/certs.php`,
+    },
+  });
+
+  const authConfig = await platform.platformAuthConfig();
+  console.log(authConfig);
+  console.log(await platform.platformPublicKey());
+};
+
+setup();
---
+++ routes.js
@@ -0,0 +1,162 @@
+const router = require("express").Router();
+const path = require("path");
+
+// Requiring Ltijs
+const lti = require("ltijs").Provider;
+
+router.post("/getGrade", async (req, res) => {
+  const idtoken = res.locals.token;
+  const response = await lti.Grade.getScores(
+    idtoken,
+    idtoken.platformContext.endpoint.lineitem,
+    { userId: req.body.userId }
+  );
+  return res.send(response);
+});
+
+// Grading route
+router.post("/grade", async (req, res) => {
+  console.log(req.body.userId + " " + req.body.grade);
+  try {
+    const idtoken = res.locals.token; // IdToken
+    const score = req.body.grade; // User numeric score sent in the body
+    // Creating Grade object
+    const gradeObj = {
+      userId: req.body.userId,
+      scoreGiven: score,
+      scoreMaximum: 100,
+      activityProgress: "Completed",
+      gradingProgress: "FullyGraded",
+    };
+
+    // Selecting linetItem ID
+    let lineItemId = idtoken.platformContext.endpoint.lineitem; // Attempting to retrieve it from idtoken
+    if (!lineItemId) {
+      const response = await lti.Grade.getLineItems(idtoken, {
+        resourceLinkId: true,
+      });
+      const lineItems = response.lineItems;
+      if (lineItems.length === 0) {
+        // Creating line item if there is none
+        console.log("Creating new line item");
+        const newLineItem = {
+          scoreMaximum: 100,
+          label: "Grade",
+          tag: "grade",
+          resourceLinkId: idtoken.platformContext.resource.id,
+        };
+        const lineItem = await lti.Grade.createLineItem(idtoken, newLineItem);
+        lineItemId = lineItem.id;
+      } else lineItemId = lineItems[0].id;
+    }
+
+    // Sending Grade
+    const responseGrade = await lti.Grade.submitScore(
+      idtoken,
+      lineItemId,
+      gradeObj
+    );
+    return res.send(responseGrade);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send({ err: err.message });
+  }
+});
+
+// Names and Roles route
+router.post("/members", async (req, res) => {
+  try {
+    const result = await lti.NamesAndRoles.getMembers(res.locals.token);
+    if (result) return res.send(result.members);
+    return res.sendStatus(500);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send(err.message);
+  }
+});
+
+// Handles deep linking request generation with the selected resource
+router.post("/deeplink", async (req, res) => {
+  console.log("POST DEEP LINKING");
+  try {
+    const resource = req.body;
+
+    const items = [
+      {
+        type: "ltiResourceLink",
+        title: resource.title,
+        url: "https://fgpe.dcc.fc.up.pt/lti-tool/" + resource.title,
+      },
+    ];
+
+    const form = await lti.DeepLinking.createDeepLinkingForm(
+      res.locals.token,
+      items,
+      { message: "Successfully Registered" }
+    );
+    if (form) return res.send(form);
+    return res.sendStatus(500);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).send(err.message);
+  }
+});
+
+router.get("/members", async (req, res) => {
+  return res.sendFile(path.join(__dirname, "../public/members.html"));
+});
+
+router.get("/home", async (req, res) => {
+  return res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/example", async (req, res) => {
+  return res.sendFile(path.join(__dirname, "../public/example.html"));
+});
+
+router.get("/deeplinking", async (req, res) => {
+  return res.sendFile(path.join(__dirname, "../public/resources.html"));
+});
+
+// Return available deep linking resources
+router.get("/resources", async (req, res) => {
+  const resources = [
+    {
+      name: "Resource1",
+      value: "value1",
+    },
+    {
+      name: "Resource2",
+      value: "value2",
+    },
+    {
+      name: "Resource3",
+      value: "value3",
+    },
+  ];
+  return res.send(resources);
+});
+
+// Get user and context information
+router.get("/info", async (req, res) => {
+  const token = res.locals.token;
+  const context = res.locals.context;
+
+  const info = {};
+  if (token.userInfo) {
+    if (token.userInfo.name) info.name = token.userInfo.name;
+    if (token.userInfo.email) info.email = token.userInfo.email;
+  }
+
+  if (context.roles) info.roles = context.roles;
+  if (context.context) info.context = context.context;
+
+  return res.send(info);
+});
+
+// Wildcard route to deal with redirecting to React routes
+router.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+);
+
+module.exports = router;
