a18d1db0ab7a03376c7032ea6bebda7e640747e5
Ricardo Queirós <ricardo.queiros@gmail.com>
2022-02-20 21:42:12 +0000

    asd

--- index.js
+++ index.js
@@ -48,28 +48,28 @@ const setup = async () => {
   app.use("/lti-tool", lti.app);
   app.listen(process.env.LTI_TOOL_PORT);
 
-/* const app = express();
-app.use("/", lti.app);
-app.listen(3000);
-*/
-/**
- * Register platform
- */
-/*   const platform = await lti.registerPlatform({
-    url: process.env.MOODLE_URL,
-    name: process.env.LTI_TOOL_NAME,
-    clientId: process.env.LTI_TOOL_CLIENT_ID,
-    authenticationEndpoint: `${process.env.MOODLE_URL}/mod/lti/auth.php`,
-    accesstokenEndpoint: `${process.env.MOODLE_URL}/mod/lti/token.php`,
-    authConfig: {
-      method: 'JWK_SET',
-      key: `${process.env.MOODLE_URL}/mod/lti/certs.php`
-    }
-  })
- 
-  const authConfig = await platform.platformAuthConfig();
-  console.log(authConfig);
-  console.log(await platform.platformPublicKey()) 
+  /* const app = express();
+  app.use("/", lti.app);
+  app.listen(3000);
+  */
+  /**
+   * Register platform
+   */
+  /*   const platform = await lti.registerPlatform({
+      url: process.env.MOODLE_URL,
+      name: process.env.LTI_TOOL_NAME,
+      clientId: process.env.LTI_TOOL_CLIENT_ID,
+      authenticationEndpoint: `${process.env.MOODLE_URL}/mod/lti/auth.php`,
+      accesstokenEndpoint: `${process.env.MOODLE_URL}/mod/lti/token.php`,
+      authConfig: {
+        method: 'JWK_SET',
+        key: `${process.env.MOODLE_URL}/mod/lti/certs.php`
+      }
+    })
+   
+    const authConfig = await platform.platformAuthConfig();
+    console.log(authConfig);
+    console.log(await platform.platformPublicKey()) */
 }
 
 setup()
