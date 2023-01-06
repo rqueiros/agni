5855c542d702c7fb8b95270e422be091c2b51595
YannikBauer1 <yannikbauer.1@gmail.com>
2023-01-06 15:07:22 +0100

    server.js change

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: "https://agni.dcc.fc.up.pt/strapi",
   app: {
     keys: env.array('APP_KEYS'),
   },
