24bc859b7cd6b67334c5db7d347c580e0d2581ec
YannikBauer1 <yannikbauer.1@gmail.com>
2023-07-14 15:59:37 +0100

    domain + vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
   //publicPath: 'http://localhost:8080'
-  devServer: {
-    host: '0.0.0.0',
-    https: false,
-    port: 8080,
-    public: 'http://0.0.0.0'
-  },
+  //devServer: {
+  //  host: '0.0.0.0',
+  //  https: false,
+  //  port: 8080,
+  //  public: 'http://0.0.0.0'
+  //},
   publicPath: process.env.NODE_ENV === "production" ? "/learnjs/" : "/"
 };
